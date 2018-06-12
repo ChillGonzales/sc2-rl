8343e3c7bb18420541ce655ed2d0038dc54b8c82
Colin Monroe <cmmonroe2010@gmail.com>
2018-06-12 17:40:53 -0400

    added argument training

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -14,13 +14,16 @@ _PLAYER_ENEMY = features.PlayerRelative.ENEMY
 
 FUNCTIONS = actions.FUNCTIONS
 
+
 def _xy_locs(mask):
-  """Mask should be a set of bools from comparison with a feature layer."""
-  y, x = mask.nonzero()
-  return list(zip(x, y))
+    """Mask should be a set of bools from comparison with a feature layer."""
+    y, x = mask.nonzero()
+    return list(zip(x, y))
+
 
 class DDPGAgent(object):
     """A Deep Deterministic Policy Gradient implementation of an SC2 agent."""
+
     def __init__(self, action_spec):
         self.action_spec = action_spec
 
@@ -36,18 +39,23 @@ class DDPGAgent(object):
                 pass
             elif 'adaptive-param' in current_noise_type:
                 _, stddev = current_noise_type.split('_')
-                param_noise = AdaptiveParamNoiseSpec(initial_stddev=float(stddev), desired_action_stddev=float(stddev))
+                param_noise = AdaptiveParamNoiseSpec(initial_stddev=float(
+                    stddev), desired_action_stddev=float(stddev))
             elif 'normal' in current_noise_type:
                 _, stddev = current_noise_type.split('_')
-                action_noise = NormalActionNoise(mu=np.zeros(nb_actions), sigma=float(stddev) * np.ones(nb_actions))
+                action_noise = NormalActionNoise(mu=np.zeros(
+                    nb_actions), sigma=float(stddev) * np.ones(nb_actions))
             elif 'ou' in current_noise_type:
                 _, stddev = current_noise_type.split('_')
-                action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(nb_actions), sigma=float(stddev) * np.ones(nb_actions))
+                action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(
+                    nb_actions), sigma=float(stddev) * np.ones(nb_actions))
             else:
-                raise RuntimeError('unknown noise type "{}"'.format(current_noise_type))
+                raise RuntimeError(
+                    'unknown noise type "{}"'.format(current_noise_type))
 
         # Configure components.
-        self.memory = Memory(limit=int(50), action_shape=(nb_actions, ), observation_shape=obs_shape)
+        self.memory = Memory(limit=int(50), action_shape=(
+            nb_actions, ), observation_shape=obs_shape)
         self.critic = Critic(layer_norm=layer_norm)
         self.actor = Actor(nb_actions, layer_norm=layer_norm)
 
@@ -55,23 +63,23 @@ class DDPGAgent(object):
 
         # max_action = env.action_space.high
         self.ddpg = DDPG(actor=self.actor, critic=self.critic, memory=self.memory, observation_shape=obs_shape,
-            action_shape=(nb_actions, ), gamma=gamma, tau=tau, action_noise=action_noise, param_noise=param_noise)
+                         action_shape=(nb_actions, ), gamma=gamma, tau=tau, action_noise=action_noise, param_noise=param_noise)
 
     def step(self, obs, available_actions):
         acts, q = self.ddpg.pi(obs, apply_noise=True, compute_Q=True)
-        # TODO: Do this with softmax. Also maybe invert the final tanh?
-        z = (2 - (acts + 1)) / 2
-        selected = int(z * self.total_actions)
-        # print(acts)
-        # print(selected)
+        # Move distribution from [-1, 1] to [0, 2] then scale it out to the total action space to select
+        normalized_actions = (2 - (acts + 1)) / 2
+        # First index of actions is the function id, rest are argument values
+        selected = int(normalized_actions[0] * self.total_actions)
+        # If our choice isn't available then just take a random action.
+        # TODO: Should we be masking the unavailable actions and then selecting based on that distribution?
         if (selected in available_actions):
             function_id = selected
-            # print("Chosen:", selected)
         else:
             function_id = np.random.choice(available_actions)
-            # print("Random:", function_id)
-        args = [[np.random.randint(0, size) for size in arg.sizes]
-                for arg in self.action_spec[0].functions[function_id].args]
+        required_args = self.action_spec[0].functions[function_id].args
+        args = [[int(normalized_actions[i] * size) for size in required_args[i].sizes]
+                         for i in range(len(required_args))]
         return actions.FunctionCall(function_id, args), q, function_id, selected in available_actions
 
     def reset(self):
@@ -79,6 +87,6 @@ class DDPGAgent(object):
 
     def initialize(self, sess):
         self.ddpg.initialize(sess)
-    
+
     def store_transition(self, obs, action, r, new_obs, done):
         self.ddpg.store_transition(obs, action, r, new_obs, done)
--- run_main.py
+++ run_main.py
@@ -13,144 +13,152 @@ import tensorflow as tf
 Dimensions = features.Dimensions
 AgentInterfaceFormat = features.AgentInterfaceFormat
 OBS_DIM = 680012
-ACT_DIM = 1 
+ACT_DIM = 7
 TOTAL_FN = 451
 
+
 def flattenFeatures(obs):
-  flat_list = np.array([]) 
-  for feature in ['feature_screen', 'available_actions', 'action_result', 'single_select', 'multi_select']:
-    for lst in obs[feature]:
-      flat_list = np.append(flat_list, lst)
-  flat_list.flatten()
-  return flat_list
+    flat_list = np.array([])
+    for feature in ['feature_screen', 'available_actions', 'action_result', 'single_select', 'multi_select']:
+        for lst in obs[feature]:
+            flat_list = np.append(flat_list, lst)
+    flat_list.flatten()
+    return flat_list
+
 
 def runAgent(agent, game, nb_epochs, nb_rollout_steps):
-  # assert (np.abs(env.action_space.low) == env.action_space.high).all()  # we assume symmetric actions.
-  with U.single_threaded_session() as sess:
-    agent.initialize(sess)
-    sess.graph.finalize()
-    step = 0
-    episode = 0
-    eval_episode_rewards_history = deque(maxlen=100)
-    episode_rewards_history = deque(maxlen=100)
-    # Prepare everything.
-    agent.reset()
-    obs = game.reset()[0] # Only care about 1 agent right now
-    features, available_actions = flattenFeatures(obs.observation), obs.observation.available_actions
-    done = False
-    episode_reward = 0.
-    episode_step = 0
+    with tf.Session() as sess:
+        agent.initialize(sess)
+        sess.graph.finalize()
+        step = 0
+        episode = 0
+        eval_episode_rewards_history = deque(maxlen=100)
+        episode_rewards_history = deque(maxlen=100)
+        # Prepare everything.
+        agent.reset()
+        obs = game.reset()[0]  # Only care about 1 agent right now
+        features, available_actions = flattenFeatures(
+            obs.observation), obs.observation.available_actions
+        done = False
+        episode_reward = 0.
+        episode_step = 0
 
-    start_time = time.time()
+        start_time = time.time()
 
-    epoch_episode_rewards = []
-    epoch_episode_steps = []
-    epoch_episode_eval_rewards = []
-    epoch_episode_eval_steps = []
-    epoch_start_time = time.time()
-    epoch_actions = []
-    epoch_qs = []
-    epoch_episodes = 0
-    chosen_count = 0
-    for epoch in range(nb_epochs):
-      print("Starting epoch ", epoch)
-      # Perform rollouts.
-      for t in range(nb_rollout_steps):
-        # Predict next action.
-        action, q, action_index, chosen = agent.step(features, available_actions)
-        # assert action.shape == game.action_spec
-        # print("Action: ", action)
-        if chosen:
-          chosen_count += 1
+        epoch_episode_rewards = []
+        epoch_episode_steps = []
+        epoch_episode_eval_rewards = []
+        epoch_episode_eval_steps = []
+        epoch_start_time = time.time()
+        epoch_actions = []
+        epoch_qs = []
+        epoch_episodes = 0
+        chosen_count = 0
+        for epoch in range(nb_epochs):
+            print("Starting epoch ", epoch)
+            # Perform rollouts.
+            for t in range(nb_rollout_steps):
+                # Predict next action.
+                action, q, action_index, chosen = agent.step(
+                    features, available_actions)
+                # print("Action: ", action)
+                if chosen:
+                    chosen_count += 1
 
-        # assert max_action.shape == action.shape
-        try:
-          new_obs = game.step([action])[0]
-        except ValueError:
-          new_obs = game.step([actions.FunctionCall(0, [])])[0]
-        new_features, r, available_actions = new_obs.observation, new_obs.observation.score_cumulative[0], new_obs.observation.available_actions
-        new_features = flattenFeatures(new_features)[:OBS_DIM]
-        
-        episode_reward += r
-        episode_step += 1
+                try:
+                    new_obs = game.step([action])[0]
+                except ValueError:
+                    new_obs = game.step([actions.FunctionCall(0, [])])[0]
 
-        # Book-keeping.
-        epoch_actions.append(action)
-        epoch_qs.append(q)
-        agent.store_transition(features, action_index, r, new_features, done)
-        obs = new_obs
-        features = new_features
+                new_features, r, available_actions = new_obs.observation, new_obs.observation.score_cumulative[
+                    0], new_obs.observation.available_actions
+                new_features = flattenFeatures(new_features)[:OBS_DIM]
 
-        if obs.last():
-          # Episode done.
-          epoch_episode_rewards.append(episode_reward)
-          episode_rewards_history.append(episode_reward)
-          epoch_episode_steps.append(episode_step)
-          print("Epoch", epoch, "complete. Total reward:", episode_reward, ". Chosen percent:", (chosen_count / t) * 100, ". Steps taken:", t, ". Win:", obs.reward != -1)
-          episode_reward = 0.
-          episode_step = 0
-          chosen_count = 0
-          epoch_episodes += 1
-          agent.reset()
-          obs = game.reset()
-          break
+                episode_reward += r
+                episode_step += 1
 
-      # Train.
-      epoch_actor_losses = []
-      epoch_critic_losses = []
-      epoch_adaptive_distances = []
-      nb_train_steps = 100
-      batch_size = 25
-      param_noise_adaptation_interval = 25
-      print("Training network...")
-      for t_train in range(nb_train_steps):
-        # Adapt param noise, if necessary.
-        if agent.memory.nb_entries >= batch_size and t_train % param_noise_adaptation_interval == 0:
-            distance = agent.ddpg.adapt_param_noise()
-            epoch_adaptive_distances.append(distance)
+                # Book-keeping.
+                epoch_actions.append(action)
+                epoch_qs.append(q)
+                agent.store_transition(
+                    features, action_index, r, new_features, done)
+                obs = new_obs
+                features = new_features
 
-        cl, al = agent.ddpg.train()
-        epoch_critic_losses.append(cl)
-        epoch_actor_losses.append(al)
-        agent.ddpg.update_target_net()
+                if obs.last():
+                    # Episode done.
+                    epoch_episode_rewards.append(episode_reward)
+                    episode_rewards_history.append(episode_reward)
+                    epoch_episode_steps.append(episode_step)
+                    print("Epoch", epoch, "complete. Total reward:", episode_reward, ". Chosen percent:",
+                          (chosen_count / t) * 100, ". Steps taken:", t, ". Win:", obs.reward != -1)
+                    episode_reward = 0.
+                    episode_step = 0
+                    chosen_count = 0
+                    epoch_episodes += 1
+                    agent.reset()
+                    obs = game.reset()
+                    break
 
-      # Evaluate.
-      # eval_episode_rewards = []
-      # eval_qs = []
-      # if eval_env is not None:
-      #   eval_episode_reward = 0.
-      #   for t_rollout in range(nb_eval_steps):
-      #     eval_action, eval_q = agent.pi(eval_obs, apply_noise=False, compute_Q=True)
-      #     eval_obs, eval_r, eval_done, eval_info = eval_env.step(max_action * eval_action)  # scale for execution in env (as far as DDPG is concerned, every action is in [-1, 1])
-      #     if render_eval:
-      #       eval_env.render()
-      #     eval_episode_reward += eval_r
+            # Train.
+            epoch_actor_losses = []
+            epoch_critic_losses = []
+            epoch_adaptive_distances = []
+            nb_train_steps = 100
+            batch_size = 25
+            param_noise_adaptation_interval = 25
+            print("Training network...")
+            for t_train in range(nb_train_steps):
+                # Adapt param noise, if necessary.
+                if agent.memory.nb_entries >= batch_size and t_train % param_noise_adaptation_interval == 0:
+                    distance = agent.ddpg.adapt_param_noise()
+                    epoch_adaptive_distances.append(distance)
 
-      #     eval_qs.append(eval_q)
-      #     if eval_done:
-      #       eval_obs = eval_env.reset()
-      #       eval_episode_rewards.append(eval_episode_reward)
-      #       eval_episode_rewards_history.append(eval_episode_reward)
-      #       eval_episode_reward = 0.  
+                cl, al = agent.ddpg.train()
+                epoch_critic_losses.append(cl)
+                epoch_actor_losses.append(al)
+                agent.ddpg.update_target_net()
+
+            # Evaluate.
+            # eval_episode_rewards = []
+            # eval_qs = []
+            # if eval_env is not None:
+            #   eval_episode_reward = 0.
+            #   for t_rollout in range(nb_eval_steps):
+            #     eval_action, eval_q = agent.pi(eval_obs, apply_noise=False, compute_Q=True)
+            #     eval_obs, eval_r, eval_done, eval_info = eval_env.step(max_action * eval_action)  # scale for execution in env (as far as DDPG is concerned, every action is in [-1, 1])
+            #     if render_eval:
+            #       eval_env.render()
+            #     eval_episode_reward += eval_r
+
+            #     eval_qs.append(eval_q)
+            #     if eval_done:
+            #       eval_obs = eval_env.reset()
+            #       eval_episode_rewards.append(eval_episode_reward)
+            #       eval_episode_rewards_history.append(eval_episode_reward)
+            #       eval_episode_reward = 0.
+
 
 def main():
-  dims = Dimensions(screen=(200, 200), minimap=(50, 50))
-  format = AgentInterfaceFormat(feature_dimensions=dims)
-  game = SC2Env(map_name="Simple64",
-                agent_interface_format=format,
-                visualize=False)
+    dims = Dimensions(screen=(200, 200), minimap=(50, 50))
+    format = AgentInterfaceFormat(feature_dimensions=dims)
+    game = SC2Env(map_name="Simple64",
+                  agent_interface_format=format,
+                  visualize=False)
 
-  agent = DDPGAgent(game.action_spec())
-  obs_shape = (OBS_DIM, )
-  nb_actions = ACT_DIM
-  agent.setup(obs_shape, nb_actions, TOTAL_FN, noise_type="adaptive-param_0.01,ou_0.01")
-  runAgent(agent, game, 100, 10000)
-  # feature_keys = ['single_select', 'multi_select', 'build_queue', 'cargo', 'cargo_slots_available', 'feature_screen', 
-  #   'feature_minimap', 'last_actions', 'action_result', 'alerts', 'game_loop', 'score_cumulative', 'player', 'control_groups', 'available_actions']
+    agent = DDPGAgent(game.action_spec())
+    obs_shape = (OBS_DIM, )
+    nb_actions = ACT_DIM
+    agent.setup(obs_shape, nb_actions, TOTAL_FN,
+                noise_type="adaptive-param_0.01,ou_0.01")
+    runAgent(agent, game, 100, 10000)
+    # feature_keys = ['single_select', 'multi_select', 'build_queue', 'cargo', 'cargo_slots_available', 'feature_screen',
+    #   'feature_minimap', 'last_actions', 'action_result', 'alerts', 'game_loop', 'score_cumulative', 'player', 'control_groups', 'available_actions']
+
 
 if __name__ == "__main__":
-  import sys
-  from absl import flags
-  FLAGS = flags.FLAGS
-  FLAGS(sys.argv)
-  main()
+    import sys
+    from absl import flags
+    FLAGS = flags.FLAGS
+    FLAGS(sys.argv)
+    main()
